389de2865c948f9af73cebe0c419cfdaa4f95f73
Christopher Taylor <chrispdtaylor@sky.com>
2020-03-24 13:40:46 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,12 +8,12 @@ variable "secret_key" {}
 ##			Provider		   ##
 #################################
 provider "aws" {
-	region = "us-east-1"
+	region = "eu-west-1"
 	access_key = "${var.access_key}"
 	secret_key = "${var.secret_key}"
 }
 
-resource "aws_instance" "example" {
+resource "aws_instance" "chris-tf-test-1" {
   ami           = "ami-b374d5a5"
   instance_type = "t2.micro"
 }
